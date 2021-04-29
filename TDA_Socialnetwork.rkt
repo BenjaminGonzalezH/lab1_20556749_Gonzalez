5e5334ac2916e065e4a37ada02ae696543cd6db9
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-04-29 19:44:22 -0400

    Agregado de archivo de TDA (solo comentario introductorio)

---
+++ TDA_Socialnetwork.rkt
@@ -0,0 +1,8 @@
+; DESCRIPCION GENERAL.
+; Este archivo guarda el TDA socialnetwork y sus funciones relacionadas,
+; el proposito de esto es documentar la estructura interna del tipo de dato
+; abstracto pensado para la implementación del lab.Además de documentar sus
+; funciones propias.
+
+; Se le da a entender al interprete el uso del lenguaje scheme.
+#lang scheme

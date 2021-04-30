1855129633f83ab8034d7f38890a77626172cee0
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-04-30 17:27:47 -0400

    Creación y documentación de archivo constructor

---
+++ Constructor_socialnetwork.rkt
@@ -0,0 +1,11 @@
+; DESCRIPCION GENERAL.
+; En este archivo se guarda la función constructor
+; del TDA socialnetwork.
+
+;Se da a entender uso del lenguaje scheme al interprete.
+#lang scheme
+
+; Socialnetwork
+; Función que crea a la red social con el uso del nombre de la
+; red social, la fecha de creación y el uso de las funciones de
+; encriptado. La función debe devolver el TDA red social.

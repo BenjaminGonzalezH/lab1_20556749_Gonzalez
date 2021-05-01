43b3ae44f39202fb718e9ab5a61f60d6e53ca764
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-01 16:18:30 -0400

    Construcción beta de la función constructor

--- Constructor_socialnetwork.rkt
+++ Constructor_socialnetwork.rkt
@@ -1,11 +1,16 @@
-; DESCRIPCION GENERAL.
+; DESCRIPCION GENERAL. 
 ; En este archivo se guarda la función constructor
 ; del TDA socialnetwork.
 
-;Se da a entender uso del lenguaje scheme al interprete.
+; Se da a entender uso del lenguaje scheme al interprete.
 #lang scheme
+
+; Se pide información del archivo "TDA_socialnetwork.rkt"
+(require "TDA_socialnetwork.rkt")
 
 ; Socialnetwork
 ; Función que crea a la red social con el uso del nombre de la
 ; red social, la fecha de creación y el uso de las funciones de
 ; encriptado. La función debe devolver el TDA red social.
+
+(define (socialnetwork name date EncryptFuntion DecryptFunction) (list name date EncryptFuntion DecryptFunction))

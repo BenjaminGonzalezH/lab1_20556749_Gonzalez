717c56d37da70b912863e9d96059bbdd9a1dd4d9
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-03 19:12:29 -0400

    Creación de funciones CreatePost, Liked, NewShare y Shared

--- SubTDA_Post.rkt
+++ SubTDA_Post.rkt
@@ -15,3 +15,43 @@
 ; a quienes se compartió y los comentarios (listas de string y otra subestructura).
 
 ; FUNCIONES PROPIAS.
+
+; CreatePost.
+; Se define función que crea un post. Por corrificación en
+; implementación no se establecen condiciones.
+
+;Se define entrada.
+(define (CreatePost User ID Content likes Mencion share comments)
+  ;Se construye lista de elementos.
+  (list User ID Content likes Mencion share comments))
+
+; Liked.
+; Función que agrega un like al post.
+
+;Se definen entradas.
+(define (Liked post)
+  ;Se agrega like al post.
+  (CreatePost (list-ref post 0) (list-ref post 1) (list-ref post 2) (+ (list-ref post 3) 1)
+              (list-ref post 4) (list-ref post 5) (list-ref post 6))
+  )
+
+; NewShare
+; Función que añade un nuevo usuario compartido.
+
+;Se definen entradas.
+(define (NewShare post username)
+  ;Se agrega like al post.
+  (CreatePost (list-ref post 0) (list-ref post 1) (list-ref post 2) (list-ref post 3)
+              (list-ref post 4) (append (list-ref post 5) (list username)) (list-ref post 6))
+  )
+
+; Shared.
+; Función que entrega las veces que se compartio
+; un post.
+
+;Se define entrada.
+(define (Shared post)
+  ;Se entrega el numero de veces
+  ;compartido como largo de lista share.
+  (length (list-ref post 5))
+  )

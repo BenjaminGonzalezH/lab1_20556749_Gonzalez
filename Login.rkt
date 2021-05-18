9fa29ed6a06eeac3ab0091779fe4ed884e8f5a23
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-18 18:35:22 -0400

    Construcción de función login, aun no terminada, falta aplicarla a funciones

--- Login.rkt
+++ Login.rkt
@@ -12,10 +12,55 @@
 (require "Constructor_socialnetwork.rkt")
 (require "Register.rkt")
 
+; ValidUser
+; Se define función que compruaba si un usuario existe en la
+; red social. Esta función recibe como entrada la lista de usuarios
+; de red social, el nombre de usuario, contraseña. Se da de salida
+; un booleano.
+(define (ValidUser list user pass)
+  ;Se define caso base.
+  (if (or (= (length list) 0) (equal? (car list) null))
+      ;Caso verdadero.
+      ;Si no se encontro ningún usuario existente
+      ;entonces se retorna falso.
+      (and #f)
+
+      ;Caso falso.
+      ;Se comprueba si el usuario y contraseña
+      ;coinciden.
+      (if (and (string=? (car (car list)) user)
+               (string=? (car (cdr (car list))) pass))
+          ;Caso verdadero.
+          ;Se retorna una operación que da verdadero.
+          (and #t)
+
+          ;Caso falso.
+          ;Se llama de nuevo a la función.
+          (ValidUser (cdr list) user pass)
+          )
+      )
+  )
+
 ; Login
 ; Se define la función login como aquella que comprueba el
 ; inicio de sesion del usuario y posteriormente entregara una función
 ; currificada a la que se hizo referencia como comando, en el
 ; caso de que la sesión "iniciada" no pertenesca a socialnetwork, esta solo
 ; entrega la función.
-(define (login x) (x))
+
+; Se define entrada de la función.
+(define (login socialn user pass comand)
+  ;Se comprueba si el usuario existe en
+  ;la red social.
+  (if (and (IsSocialnetwork socialn) (string? user)
+       (string? pass) (procedure? comand)
+       (ValidUser (list-ref socialn 4) user pass))
+      ;Caso verdadero.
+      ;Se devuelve función currificada.
+      (pass)
+
+      ;Caso falso.
+      ;Se devuelve función sin modificación.
+      (comand)
+      )
+  )

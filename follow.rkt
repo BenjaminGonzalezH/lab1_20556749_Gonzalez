65d0dced402051abcea4a61cad990341d2f43f73
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-20 11:21:24 -0400

    termino de versión beta de función follow

--- follow.rkt
+++ follow.rkt
@@ -11,13 +11,32 @@
 (require "Constructor_socialnetwork.rkt")
 (require "SubTDA_Post.rkt")
 (require "Register.rkt")
+(require "post.rkt")
 
 ; follow
 ; Función currificada que permite a un usuario seguir a una persona
 ; esto viendose reflejado en socialnetwork
-(define follow (lambda (socialn) (lambda (user) (lambda (date user)
+(define follow (lambda (socialn) (lambda (user) (lambda (date User)
                                                   ;se comprueban si los datos ingresados
                                                   ;son correctos y si el usuario existe a seguir
                                                   ;cumple que no sea el usuario mismo y este exista
                                                   ;en la lista de usuarios del socialnetwork
-                                                  (if (and (IsDate date) (string? user) 
+                                                  (if (and (IsDate date) (string? User)
+                                                           (not(ExistBefore (list-ref socialn 4) User))
+                                                           (not (string=? (car user) User)))
+                                                      ;Caso verdadero
+                                                      ;se crea otro socialnetwork con el nuevo follow.
+                                                      (list (list-ref socialn 0) (list-ref socialn 1)
+                                                            (list-ref socialn 2) (list-ref socialn 3)
+                                                            (ModifiUser (list-ref socialn 4) (NewFollower user User) '())
+                                                            (list-ref socialn 5))
+                                                            
+
+                                                      ;Caso falso.
+                                                      ;Se da socialnetwork sin cambios.
+                                                      (socialn)
+                                                      )
+                                                  )
+                                   )
+                 )
+  )

7155a8d738bedc6622785b85bd3864fc930237ad
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-20 17:26:35 -0400

    Creación de archivo comment

---
+++ comment.rkt
@@ -0,0 +1,12 @@
+; DESCRIPCION GENERAL.
+; Se establece archivo que guarda función comment.
+
+; Se le da a entender al interprete el uso de lenguaje
+; scheme.
+#lang scheme
+
+; BLOQUE DE IMPORTACIONES
+
+; Comment
+; Función que agrega un comentario a una publicación.
+(define comment '())

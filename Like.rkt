34cbeb2a2b443d00767e7605ca88b4fe05a6ef74
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-21 16:36:26 -0400

    Creación de archivo que guarda función like

---
+++ Like.rkt
@@ -0,0 +1,14 @@
+; DESCRIPCIÓN GENERAL.
+; Archivo que guarda la función like.
+
+; Se le da a entender al interprete el uso de
+; lenguaje scheme.
+#lang scheme
+
+; BLOQUE DE IMPORTACIONES.
+
+
+; Like
+; Función que da like a una publicación y guardarla en
+; socialnetwork.
+(define Like '())

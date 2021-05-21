d2edb2ad677db4f1506bc504b9dcd57254974de4
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-21 17:51:41 -0400

    Creaciuón de archivo que guarda la función like

---
+++ influencers.rkt
@@ -0,0 +1,11 @@
+; DESCRIPCIÓN GENERAL.
+; En este archivo se guardan la funcion influencers.
+
+; Se le da a entender al interprete el uso de lenguaje scheme.
+#lang scheme
+
+; Influencers.
+; Se define función influencers como aquella
+; que rankea a los usuarios con una función
+; secundarias de rankeo.
+(definen Influencers '())

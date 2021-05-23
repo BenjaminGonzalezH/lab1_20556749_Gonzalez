04e3d2c9255583c1736c2e4cf0c7b3174e3cd27e
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-23 18:41:21 -0400

    Modificación de comentarios, cambio en función register para tener un socialnetwork de salida valido y cambio en condicionales de función ExistBefore por mal uso de constante null

--- Register.rkt
+++ Register.rkt
@@ -1,6 +1,5 @@
 ; DESCRIPCIÓN GENERAL.
-; En este archivo se guarda la función  register
-; la cual construye los usuarios para el TDA socialnetwork.
+; En este archivo se guarda la función register.
 
 ; Se le da a entender al interprete el uso de lenguaje scheme.
 #lang scheme
@@ -11,14 +10,20 @@
 (require "TDA_Socialnetwork.rkt")
 (require "Constructor_socialnetwork.rkt")
 
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 ; ExistBefore.
 ; Función que comprueba si un usuario existe con
-; anterioridad en una socialnetwork. Su entrada consta
-; de la lista de usuarios del socialnetwork y el nombre del
-; usuario que quiere registrarse. Retorna booleanos.
+; anterioridad en una socialnetwork en base a nombres.
+;DOMINIO: List of Users x String.
+;RECORRIDO: Bool.
+;RECURSIÓN: Natural.
+;NOTA: el booleano entregado sigue la pregunta
+;"¿El nombre dado por entrada no existe en ningún
+;usuario de la lista?"
+
 (define (ExistBefore lista nombre)
-  ;Se establece caso base de recursión
-  (if (or (= (length lista) 0) (equal? (car lista) null))
+  ;Se establece caso base de recursión de la función.
+  (if (= (length lista) 0)
       ;Caso verdadero.
       ;Si no se encontro ningun usuario
       ;igual al usuario que se desea ingresar se
@@ -27,10 +32,10 @@
 
       ;Caso falso.
       ;Se comprueba si el elemento actual es
-      ;igual al nombre .
+      ;igual al nombre.
       (if (string=? (car (car lista)) nombre)
           ;Caso verdadero
-          ;Se retorna una operación que da false.
+          ;Se retorna una operación que da falso.
           (and #f)
 
           ;Caso falso.
@@ -42,36 +47,39 @@
 
 ; Register.
 ; Función que registra un nuevo usuario en el TDA socialnetwork.
-; Esta función debe recibir el TDA socialnetwork , la fecha, nombre y contraseña.
-; Debe de devolver el TDA socialnetwork.
+;DOMINIO: socialnetwork x date x user x string.
+;RECORRIDO: socialnetwork.
 
 (define (register socialn date User password)
-  ; Se establecen condiciones para inicializar
-  ; el trabajo de la función
+  ; Se comprueban si los elementos de entrada ingresados
+  ; son correctos, para modificar el socialnetwork.
   (if (and (IsSocialnetwork socialn) (IsDate date)
            (string? User) (string? password))
       ;Caso verdadero
       ;Se aplica función recursiva que
       ;asiste a register para comprobar existencia
-      ;de usuario similar.
+      ;de usuario con igual nombre.
       (if (ExistBefore (list-ref socialn 4) User)
           ;Caso verdadero.
-          ;Se guarda cambio en otra estructura.
+          ;Se crea otra estructura con el nuevo usuario creado.
           (list (list-ref socialn 0) (list-ref socialn 1) EncryptFunction DecryptFunction ;Elementos copiados
                 (append (list-ref socialn 4) (list(Create_User User password date '() '() '()))) ;dato modificado
                 (list-ref socialn 5)) ;Dato copiado
 
           ;Caso falso
-          ;se entrega socialnetwork vacio.
-          (socialnetwork "" '(1 3 1956) EncryptFunction EncryptFunction)
+          ;se entrega socialnetwork de error.
+          (list "Error" '(14 5 1929) EncryptFunction DecryptFunction '() '())
           )
       ;Caso falso
-      ;Se entrega un socialnetwork vacio.
-      (socialnetwork "" '(1 3 1956) EncryptFunction EncryptFunction)
+      ;Se entrega un socialnetwork de error.
+      (list "Error" '(14 5 1929) EncryptFunction DecryptFunction '() '())
       )
   )
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 ; BLOQUE DE EXPORTACION.
-; En esta sección se exportan las funciones para otros archivos.
+; En este bloque se especifican las funciones las cuales se van a
+; exportar a otros archivos.
 (provide ExistBefore)
 (provide register)

65c644ba7464aa13e87d028a06a0484e0952e31c
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-23 17:21:22 -0400

    modificación de comentarios, arreglo de función NewFollowing para respetar estructura User en la cración y arreglo en salida de funciones para respetar recorrido

--- SubTDA_User.rkt
+++ SubTDA_User.rkt
@@ -5,6 +5,9 @@
 ; Se le da entender al interprete el uso de lenguaje scheme.
 #lang scheme
 
+; Se pide información de funciones del archivo "TDA_socialnetwork.rkt"
+(require "TDA_socialnetwork.rkt")
+
 ; La subestructura user posee los siguientes elementos.
 
 (define user '("name" "password" '(date) '(followers) '(following) '(IDpublications)))
@@ -17,38 +20,35 @@
 
 ;FUNCIONES PROPIAS.
 
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+; CONSTRUCTOR.
 ; Create_User.
-; Función que recibe los elementos para crear la estructura del usuario
-; con sus elementos. No requiere de comprobación en otros elementos por
-; implementación bajo currificación.
+; Función que crea el TDA user.
+;DOMINIO: String x String x Date.
+;RECORRIDO: User.
 
-; Se define entrada.
-(define (Create_User Nickname password date followers followings IDpublications)
-  ;Se estabñece condiciones de datos.
-  (if (and (string? Nickname) (string? password))
+(define (Create_User Nickname password date followers followings IDs)
+  ;Se comprueban que los datos entregados sean correctos
+  ;para crear TDA user.
+  (if (and (string? Nickname) (string? password) (IsDate date))
      ;Caso verdadero.
-      ;De los elementos de entrada se crea una lista.
-      (list Nickname password date followers followings IDpublications)
+      ;Se crea usuario.
+      (list Nickname password date followers followings IDs)
      ;Caso falso
-      ;Se entrega lista de lista vacia.
-      (list '())
+      ;Se entrega TDA user de error
+      ;el cual tendra un ID de publicación
+      ;3000.
+      (list "Error" "alohawaii" '(14 5 1930) '() '() '(3000))
   ))
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-; AreSimilar
-; Función que comprueba si 2 usuarios tienen el mismo nombre
-; recibe como entrada 2 usuarios y entrega un true o false.
-
-; Se define entradas
-(define (AreSimilar User1 User2)
-  ;Se comprueba si los nombres son
-  ;iguales usando la función de
-  ;comparador de Strings.
-  (string=? (list-ref User1 0)(list-ref User2 0))
-  )
-
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+; CALCULO DE NUMEROS.
 ; NumFollowers
 ; Función que entrega el numero de seguidores del
 ; usuario.
+;DOMINIO: User.
+;RECORRIDO: Integer.
 
 ;Se define entradas.
 (define (numFollowers User1)
@@ -56,61 +56,82 @@
   ;length.
   (length (list-ref User1 2))
   )
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+; MODIFICADORAS.
 ; NewID
-; Función que agrega una nueva ID de publicación del usuario.
+; Función que agrega una nueva ID de publicación a la
+; lista de ID's del usuario.
+;DOMINIO: User x Integer.
+;RECORRIDO: User.
 
-;Se definen entradas.
 (define (NewID User ID)
-  ;Se comprueba si el dato es un numero.
+  ;Se comprueba si el dato ID es un numero.
   (if (integer? ID)
       ;Caso verdadero.
       ;Se crea nuevo user con la ID agregada.
-      (Create_User  (list-ref User 0) (list-ref User 1) (list-ref User 2) (list-ref User 3) (list-ref User 4) (append (list-ref User 5) (list ID)))
+      (Create_User  (list-ref User 0) (list-ref User 1)
+                    (list-ref User 2) (list-ref User 3)
+                    (list-ref User 4) (append (list-ref User 5) (list ID))
+                    )
 
       ;Caso falso}
-      ;Se entrega una lista de lista vacia.
-      (list '())
+      ;Se entrega un User sin cambios.
+      (append User)
       ))
 
 ; NewFollower.
-; Función que recibe un nuevo usuario como seguidor.
+; Función que recibe un nuevo usuario y los
+; registra como seguidor del usuario.
+;DOMINIO: User x String.
+;RECORRIDO: User.
 
-;Se definen entradas.
 (define (NewFollower User Follower)
   ;Se establece condición para ver si
   ;follower es un string.
   (if (string? Follower)
       ;Caso verdadero
-      ;Se crea nueva estructura con el seguidor nuevo
-      (Create_User  (list-ref User 0) (list-ref User 1) (list-ref User 2) (append (list-ref User 3) (list Follower)) (list-ref User 4) (list-ref User 5))
+      ;Se crea nueva estructura con el seguidor nuevo.
+      (Create_User  (list-ref User 0) (list-ref User 1) (list-ref User 2)
+                    (append (list-ref User 3) (list Follower))
+                    (list-ref User 4) (list-ref User 5)
+                    )
 
       ;Caso falso}
-      ;Se entrega una lista de lista vacia.
-      (list '())
+      ;Se entrega un User sin cambios.
+      (append User)
       ))
 
 ; NewFollowing.
-; Función que agrega a otra persona que el usuario
-; sigue
+; Función que agrega nuevo ente seguido por
+; el usuario.
+;DOMINIO: User x String.
+;RECORRIDO: User.
+
 (define (NewFollowing User Following)
   ;Se establece condición para ver si
   ;following es un string.
   (if (string? Following)
       ;Caso verdadero
       ;Se crea nueva estructura con el seguidor agregado.
-       (Create_User  (list-ref User 0) (list-ref User 1) (list-ref User 2) (append (list-ref User 3) (list Following)) (list-ref User 4))
+       (Create_User  (list-ref User 0) (list-ref User 1) (list-ref User 2)
+                     (list-ref User 3) (append (list-ref User 4) (list Following))
+                     (list-ref User 5)
+                     )
 
       ;Caso falso}
-      ;Se entrega una lista de lista vacia.
-      (list '())
+      ;Se entrega un User sin cambios.
+      (append User)
       ))
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 ; BLOQUE DE EXPORTANCIÓN.
 ; En este bloque se especifican las funciones las cuales se van a
 ; exportar a otros archivos.
 (provide Create_User)
-(provide AreSimilar)
 (provide numFollowers)
 (provide NewID)
 (provide NewFollower)

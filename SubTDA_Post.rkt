2825a331b0e7682a3cf9d1499d51372585165419
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-25 17:02:05 -0400

    Se modifican comentarios para mayor comprensión y eliminación de función shared por no uso

--- SubTDA_Post.rkt
+++ SubTDA_Post.rkt
@@ -1,10 +1,13 @@
-; Descripción General.
-; Este archivo describe la subestructura del socialnetwork
-; '(post), desde sus elementos hasta sus funciones propias.
-
 ;Se le da a entender al interprete el uso del lenguaje scheme.
 #lang scheme
 
+; DESCRIPCIÓN GENERAL
+; Este archivo describe la subestructura del socialnetwork
+; '(post), desde sus elementos hasta sus funciones propias.
+
+; BLOQUE DE IMPORTACIÓN.  
+(require "TDA_Socialnetwork.rkt")
+(require "TDA_Fecha.rkt")
 
 ; La subestructura post posee los siguientes elementos.
 
@@ -12,24 +15,31 @@
 
 ; Esta conformada por el nombre del autor (string), la ID del post (entero)
 ; el tipo de dato de la publicación (video),el contenido (string),
-; los likes quue recibe la publicación, las menciones a quienes se compartió
+; los likes que recibe la publicación, las menciones (list de strings), a quienes se compartió
 ; y los comentarios (listas de string y otra subestructura).
+
+;---------------------------------------------------------------------
 
 ; FUNCIONES PROPIAS.
 
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+; CONSTRUCTOR.
 ; CreatePost.
-; Se define función que crea un post. Por currificación en
-; implementación no se establecen condiciones.
+; Se define función que crea un post.
+;DOMINIO: String x integer x string x string x integer x list x list x list
 
-;Se define entrada.
 (define (CreatePost User ID type Content likes Mencion share comments)
   ;Se construye lista de elementos.
   (list User ID type Content likes Mencion share comments))
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+;MODIFICADORES.
 ; Liked.
 ; Función que agrega un like al post.
+;DOMINIO: Post.
+;RECORRIDO: Post.
 
-;Se definen entradas.
 (define (Liked post)
   ;Se agrega like al post.
   (CreatePost (list-ref post 0) (list-ref post 1) (list-ref post 2) (list-ref post 3)
@@ -38,25 +48,36 @@
 
 ; NewShare
 ; Función que añade un nuevo usuario compartido.
+;DOMINIO: Post.
+;RECORRIDO: Post.
 
-;Se definen entradas.
 (define (NewShare post username)
-  ;Se agrega like al post.
+  ;Se agregan nombres de usuarios a quienes se les compartio el post.
   (CreatePost (list-ref post 0) (list-ref post 1) (list-ref post 2) (list-ref post 3)
               (list-ref post 4) (list-ref post 5)
               (append (list-ref post 6) username) (list-ref post 7))
   )
 
-; Shared.
-; Función que entrega las veces que se compartio
-; un post.
+; NewComment
+; Se define función que agrega un nuevo comentario a la publicación.
+;DOMINIO: Post.
+;RECORRIDO: Post.
 
-;Se define entrada.
-(define (Shared post)
-  ;Se entrega el numero de veces
-  ;compartido como largo de lista share.
-  (length (list-ref post 6))
+(define (NewComment post date user comment)
+  ;Se comprueban elementos de entrada.
+  (if (and (IsDate date) (string? user) (string? comment))
+      ;Caso verdadero.
+      ;Se agrega comentario a la publcación.
+      (CreatePost (list-ref post 0) (list-ref post 1) (list-ref post 2) (list-ref post 3)
+              (list-ref post 4) (list-ref post 5)
+              (list-ref post 6) (append (list-ref post 7) (list (list date user comment)))
+              )
+      ;Caso falso.
+      ;Se da post sin modificar.
+      post
+      )
   )
+;<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 ; BLOQUE DE EXPORTACIÓN.
 ; En esta sección se especifican las funciones las cuales se
@@ -64,3 +85,4 @@
 (provide CreatePost)
 (provide NewShare)
 (provide Liked)
+(provide NewComment)

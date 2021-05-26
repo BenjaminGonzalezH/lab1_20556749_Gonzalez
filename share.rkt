5bc5d19afd861f3ec49ac398a52075d110395030
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-25 23:29:20 -0400

    Se traslada función changepost y cambios en bloque de importaciones

--- share.rkt
+++ share.rkt
@@ -1,38 +1,15 @@
-; DESCRIPCIÓN GENERAL.
-; En este archivo se guarda la función share.
-
 ; Se le da a entender al interprete el uso de lenguaje
 ; scheme.
 #lang scheme
 
+; DESCRIPCIÓN GENERAL.
+; En este archivo se guarda la función share.
+
 ; BLOQUE DE IMPORTACION.
+(require "TDA_Fecha.rkt")
 (require "SubTDA_Post.rkt")
-(require "SubTDA_User.rkt")
-(require "Register.rkt")
 (require "TDA_Socialnetwork.rkt")
 (require "Constructor_socialnetwork.rkt")
-(require "follow.rkt")
-(require "post.rkt")
-
-; ChangePost
-; Se define función que cambia el elemento
-; de una lista de post por otro modificado.
-(define (ChangePost listofpost post newlist)
-  ;Lista vacia o ultimo elemento.
-  (if (= (length listofpost) 0)
-      (append newlist)
-
-      ;se ve elemento
-      (if (= (list-ref (car listofpost) 1) (list-ref post 1))
-          ;Caso verdadero reemplazo.
-          (ChangePost (cdr listofpost) post (append newlist (list post)))
-
-          ;Caso falso continua.
-          (ChangePost (cdr listofpost) post (append newlist (list (car listofpost))))
-          )
-      )
-  )
-
 
 ; Share.
 ; Función que comparte publicación a los usuarios designados
@@ -54,7 +31,7 @@
 
                                                      ;Caso falso.
                                                      ;Se entrega socialnetwork sin cambios.
-                                                     socialn
+                                                     (append socialn)
                                                      )
                                                  )
                                   )
@@ -62,4 +39,4 @@
   )
 
 ; BLOQUE DE EXPORTACIÓN.
-(provide ChangePost)
+(provide share)

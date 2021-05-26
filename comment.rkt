64e1379634b73b356ddc9964e17b1671ed72d663
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2021-05-26 09:32:27 -0400

    Ajuste en condicionales para comprobar seguidos y ajustes de entrada para ajustarse a estandar dado por proyecto

--- comment.rkt
+++ comment.rkt
@@ -7,34 +7,37 @@
 
 ; BLOQUE DE IMPORTACIONES.
 (require "SubTDA_Post.rkt")
-(require "Register.rkt")
+(require "SubTDA_User.rkt")
+(require "TDA_Fecha.rkt")
 (require "TDA_Socialnetwork.rkt")
-(require "Constructor_socialnetwork.rkt")
-(require "post.rkt")
-(require "share.rkt")
+
 
 
 ; Comment
 ; Función que agrega un comentario a una publicación.
-(define comment (lambda (socialn) (lambda (date ID coment)
+(define comment (lambda (socialn) (lambda (user) (lambda (date) (lambda (ID) (lambda (coment)
                                     ;Se establece comprobación de
                                     ;ingresión de datos
                                     (if (and (IsDate date) (number? ID)
-                                             (string? coment))
+                                             (string? coment)
+                                             ((AreFriends (WingUser user)) (car (list-ref (PostSocial socialn) ID))))
                                         ;Caso verdadero.
                                         ;Se crea nueva estructura socialnetwork.
-                                        (list (list-ref socialn 0) (list-ref socialn 1)
-                                              (list-ref socialn 2) (list-ref socialn 3)
-                                              (list-ref socialn 4)
-                                              (ChangePost (list-ref socialn 5)
-                                                          (NewComment (list-ref (list-ref socialn 5) ID) date "ella" coment)
+                                        (list (NameSocial socialn) (DateSocial socialn)
+                                              (EncrySocial socialn) (DecrySocial socialn)
+                                              (UserSocial socialn)
+                                              (ChangePost (PostSocial socialn)
+                                                          (NewComment (list-ref (PostSocial socialn) ID) date (NameUser user) coment)
                                                           '())
                                               )
 
                                         ;Caso falso.
                                         ;Se entrega socialnetwork sin cambios
-                                        socialn
+                                        (append socialn)
                                               )
                                     )
                   )
-  )
+  ))))
+
+; BLOQUE DE EXPORTACIÓN.
+(provide comment)
